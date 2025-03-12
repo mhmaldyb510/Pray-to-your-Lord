8082e06236ca1570d7ecf6ba0cd1ff837e8a0245
Mohamed Naser Mohamed Aldeeb <53155074+mhmaldyb510@users.noreply.github.com>
2025-03-12 05:28:32 +0200

    remove location services and related dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:muslim_companion/helpers/location_services.dart';
 import 'package:muslim_companion/my_app.dart';
 import 'package:quran_library/quran_library.dart';
 
 void main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
-  await LocationService().requestLocationPermission();
   await QuranLibrary().init();
   runApp(const MyApp());
 }
